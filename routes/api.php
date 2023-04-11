9d8ae402ecfd1474916bf5b0e581b91e768d63e6
leotan <leotan.haiken@gmail.com>
2023-04-11 23:26:50 +0800

    Fixed missing StudentController path

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserAuthController;
-use App\Http\Controllers;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,21 +16,6 @@ use App\Http\Controllers;
 |
 */
 
-/*
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
-
-// localhost:8000/api/Student
-/*
-Route::group(['namespace'=>'App\Http\Controllers'], function(){
-    Route::apiResources([
-        'student' => StudentController::class
-    ]);
-});
-*/
-
 // Route for registration
 Route::post('/register', [UserAuthController::class, 'register']);
 
@@ -39,5 +24,5 @@ Route::post('/login', [UserAuthController::class, 'login'])->name('login');
 
 // Routes that require authentication
 Route::middleware(['auth:api'])->group(function () {
-    Route::apiResource('/student', 'StudentController@index');
+    Route::apiResource('/student', StudentController::class);
 });
